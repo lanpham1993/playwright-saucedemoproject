1bc4812d0932c876e7fa904a7d8e95a89ffdff04
Lan pham <lanpham09111993@outlook.com.vn>
2026-03-28 08:30:23 +0100

    first commit

---
+++ tests/login.spec.js
@@ -0,0 +1,17 @@
+import { test } from '../fixtures/saucedemoPage.fixture';
+import { expect } from '@playwright/test';
+import { LoginPage } from '../pages/Login.page';
+
+test('should login successfully', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login(process.env.STANDARD_USERNAME, process.env.PASSWORD);
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+await expect(page).toHaveTitle('Swag Labs');
+}
+);
+
+test('should show locked out error message with invalid credentials', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login(process.env.LOCKED_USERNAME, process.env.PASSWORD);
+  await expect(loginPage.getErrorMessage()).toBe('Epic sadface: Sorry, this user has been locked out.');
+});
