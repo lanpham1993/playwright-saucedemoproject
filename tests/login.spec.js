3baf6e0a45c2cec69ac12e11713372fab4782e0a
Lan pham <lanpham09111993@outlook.com.vn>
2026-03-28 11:14:50 +0100

    fix error 'cannot find module'

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -6,12 +6,12 @@ test('should login successfully', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.login(process.env.STANDARD_USERNAME, process.env.PASSWORD);
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-await expect(page).toHaveTitle('Swag Labs');
+  await expect(page).toHaveTitle('Swag Labs');
 }
 );
 
 test('should show locked out error message with invalid credentials', async ({ page }) => {
   const loginPage = new LoginPage(page);
   await loginPage.login(process.env.LOCKED_USERNAME, process.env.PASSWORD);
- expect(await loginPage.getErrorMessage()).toBe('Epic sadface: Sorry, this user has been locked out.');
+  expect(await loginPage.getErrorMessage()).toBe('Epic sadface: Sorry, this user has been locked out.');
 });
